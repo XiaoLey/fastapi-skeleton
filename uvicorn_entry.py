03003c557b6f61d840198be4ffbeb4da4f6435ae
Xiao Ley <xiao.ley@outlook.com>
2024-07-01 18:09:55 +0800

    ♻️(uvicorn_entry): determination of removing main entry

--- uvicorn_entry.py
+++ uvicorn_entry.py
@@ -2,5 +2,4 @@ import uvicorn
 from config.config import settings
 
 
-if __name__ == "__main__":
-    uvicorn.run(app="main:app", host=settings.SERVER_HOST, port=settings.SERVER_PORT, headers=[("server", settings.NAME)])
+uvicorn.run(app="main:app", host=settings.SERVER_HOST, port=settings.SERVER_PORT, headers=[("server", settings.NAME)])
